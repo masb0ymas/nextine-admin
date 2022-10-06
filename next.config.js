6650037df1c42db43baeded3302b1ab34c70a094
N. Fajri <fajriajjha@gmail.com>
2022-10-06 22:15:06 +0700

    fix: config env

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
-});
+})
 
 module.exports = withBundleAnalyzer({
   reactStrictMode: false,
   eslint: {
     ignoreDuringBuilds: true,
   },
-});
+  env: {
+    BRAND: process.env.BRAND,
+  },
+})
---
+++ src/config/env.ts
@@ -0,0 +1,1 @@
+export const BRAND = process.env.BRAND ?? 'Nextine Admin'
