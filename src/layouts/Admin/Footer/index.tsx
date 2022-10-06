6482179deb857ab88857996f7ed7e7f3cdbe7a63
N. Fajri <fajriajjha@gmail.com>
2022-10-06 22:15:45 +0700

    fix: layout admin

--- src/layouts/Admin/Footer/index.tsx
+++ src/layouts/Admin/Footer/index.tsx
@@ -1,75 +1,18 @@
-import { createStyles, Container, Group, ActionIcon, Text } from '@mantine/core'
-import {
-  IconBrandTwitter,
-  IconBrandYoutube,
-  IconBrandInstagram,
-} from '@tabler/icons'
-
-const useStyles = createStyles((theme) => ({
-  footer: {
-    position: 'initial',
-    borderTop: `1px solid ${
-      theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2]
-    }`,
-  },
-
-  inner: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingTop: theme.spacing.xl,
-    paddingBottom: theme.spacing.xl,
-
-    [theme.fn.smallerThan('xs')]: {
-      flexDirection: 'column',
-    },
-  },
-
-  links: {
-    [theme.fn.smallerThan('xs')]: {
-      marginTop: theme.spacing.md,
-    },
-  },
-
-  link: {
-    ...theme.fn.focusStyles(),
-    display: 'flex',
-    alignItems: 'center',
-    textDecoration: 'none',
-    fontSize: theme.fontSizes.sm,
-    color: theme.black,
-    padding: `${theme.spacing.xs}px ${theme.spacing.sm}px`,
-    borderRadius: theme.radius.sm,
-    fontWeight: 500,
-  },
-}))
+import { Center, Footer } from '@mantine/core'
+import { BRAND } from 'config/env'
+import React from 'react'
 
 function FooterLayout() {
-  const { classes } = useStyles()
+  const dateNow = new Date()
+  const yearNow = dateNow.getFullYear()
 
   return (
-    <div className={classes.footer}>
-      <Container className={classes.inner}>
-        <Text
-          className={classes.link}
-          style={{ fontSize: 18, fontWeight: 600 }}
-        >
-          My App
-        </Text>
-
-        <Group spacing={0} className={classes.links} position="right" noWrap>
-          <ActionIcon size="lg">
-            <IconBrandTwitter size={18} stroke={1.5} />
-          </ActionIcon>
-          <ActionIcon size="lg">
-            <IconBrandYoutube size={18} stroke={1.5} />
-          </ActionIcon>
-          <ActionIcon size="lg">
-            <IconBrandInstagram size={18} stroke={1.5} />
-          </ActionIcon>
-        </Group>
-      </Container>
-    </div>
+    <Footer height={60} p="md" style={{ fontSize: 14 }}>
+      <Center>
+        &copy; {`${yearNow} Created By`}&nbsp;
+        <b>{BRAND}</b>
+      </Center>
+    </Footer>
   )
 }
 
--- src/layouts/Admin/index.tsx
+++ src/layouts/Admin/index.tsx
@@ -1,14 +1,90 @@
-import { Container } from '@mantine/core'
+import {
+  AppShell,
+  Burger,
+  Code,
+  createStyles,
+  Group,
+  Header,
+  MediaQuery,
+  Navbar,
+  ScrollArea,
+  Text,
+  useMantineTheme,
+} from '@mantine/core'
+import { BRAND } from 'config/env'
+import { useState } from 'react'
 import FooterLayout from './Footer'
-import SiderbarLayout from './Sidebar'
+import { NavbarMenus } from './NavbarMenu'
+
+const useStyles = createStyles((theme, _params, _getRef) => ({
+  version: {
+    backgroundColor: theme.fn.lighten(
+      theme.fn.variant({ variant: 'filled', color: theme.primaryColor })
+        .background!,
+      0.1,
+    ),
+    color: theme.white,
+    fontWeight: 700,
+  },
+}))
 
 function AdminLayout() {
-  return (
-    <Container my={0} mx={0} py={0} px={0} style={{ maxWidth: '100vw' }}>
-      <SiderbarLayout />
+  const theme = useMantineTheme()
+  const [opened, setOpened] = useState(false)
 
-      <FooterLayout />
-    </Container>
+  const { classes } = useStyles()
+
+  return (
+    <AppShell
+      styles={{
+        main: {
+          background:
+            theme.colorScheme === 'dark'
+              ? theme.colors.dark[8]
+              : theme.colors.gray[0],
+        },
+      }}
+      navbarOffsetBreakpoint="sm"
+      navbar={
+        <Navbar
+          p="md"
+          hiddenBreakpoint="sm"
+          hidden={!opened}
+          width={{ sm: 200, lg: 300 }}
+        >
+          <Navbar.Section grow component={ScrollArea} mx="-xs" px="xs">
+            <NavbarMenus />
+          </Navbar.Section>
+
+          <Navbar.Section>{/* Footer with user */}</Navbar.Section>
+        </Navbar>
+      }
+      header={
+        <Header height={70} p="md">
+          <div
+            style={{ display: 'flex', alignItems: 'center', height: '100%' }}
+          >
+            <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
+              <Burger
+                opened={opened}
+                onClick={() => setOpened((o) => !o)}
+                size="sm"
+                color={theme.colors.gray[6]}
+                mr="xl"
+              />
+            </MediaQuery>
+
+            <Group position="apart" spacing="lg">
+              <Text style={{ fontSize: 22, fontWeight: 600 }}>{BRAND}</Text>
+              <Code className={classes.version}>v0.0.1</Code>
+            </Group>
+          </div>
+        </Header>
+      }
+      footer={<FooterLayout />}
+    >
+      <Text>Resize app to see responsive navbar in action</Text>
+    </AppShell>
   )
 }
 
