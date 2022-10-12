2a13957dd21391992ea256976b59d6ac2d0d8ecc
N. Fajri <fajriajjha@gmail.com>
2022-10-12 18:08:23 +0700

    fix: user table

--- src/views/Admin/Settings/Account/User/index.tsx
+++ src/views/Admin/Settings/Account/User/index.tsx
@@ -1,174 +1,200 @@
-import MyTable from '@core/components/MyTable/MyTable'
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import formatPhone from '@core/helpers/Phone'
+import useDebounce from '@core/hooks/useDebounce/useDebounce'
 import {
+  ActionIcon,
   Button,
   Checkbox,
+  createStyles,
+  Divider,
   Grid,
+  Group,
   Input,
-  Pagination,
-  ThemeIcon,
+  Stack,
+  Text,
+  useMantineTheme,
 } from '@mantine/core'
-import { IconEdit, IconEyeCheck, IconPlus, IconSearch } from '@tabler/icons'
-import { createColumnHelper } from '@tanstack/react-table'
+import { openModal } from '@mantine/modals'
+import { showNotification } from '@mantine/notifications'
+import {
+  IconEdit,
+  IconPlus,
+  IconSearch,
+  IconTrash,
+  IconTrashX,
+} from '@tabler/icons'
 import { UserRelationEntity } from 'data/entities/User'
+import useUser from 'data/query/useUser'
+import dayjs from 'dayjs'
+import {
+  DataTable,
+  DataTableColumn,
+  DataTableContextMenuItemProps,
+} from 'mantine-datatable'
+import { useEffect, useState } from 'react'
 
-const data: UserRelationEntity[] = [
-  {
-    id: 'acd85619-3138-46b3-abef-0d20b6a3cd5e',
-    fullName: 'Upin',
-    isActive: true,
-    phone: '+6281234567890',
-    Role: { name: 'Super Admin' },
-    email: 'super.admin@mail.com',
+const useStyles = createStyles((theme) => ({
+  modal: { width: 800 },
+  modalTitle: {
+    color:
+      theme.colorScheme === 'dark'
+        ? theme.colors.dark[2]
+        : theme.colors.gray[6],
+    fontWeight: 700,
   },
-  {
-    id: '759cdf37-8ce4-45a8-8fc6-5d5c7c11d0cb',
-    fullName: 'Ipin',
-    isActive: true,
-    phone: '+6281234567890',
-    Role: { name: 'Admin' },
-    email: 'admin@mail.com',
-  },
-  {
-    id: 'a5bb8503-44d6-4219-be6e-3f295dd193fd',
-    fullName: 'Ehsan',
-    isActive: false,
-    phone: null,
-    Role: { name: 'User' },
-    email: 'user@mail.com',
-  },
-  {
-    id: 'de8de118-82aa-4684-aea7-2a0da99e8508',
-    fullName: 'Fizi',
-    isActive: true,
-    phone: null,
-    Role: { name: 'Guest' },
-    email: 'guest@mail.com',
-  },
-  {
-    id: '0f802cd0-b669-4b12-a787-86e805e9d982',
-    fullName: 'Mail',
-    isActive: true,
-    phone: null,
-    Role: { name: 'Owner' },
-    email: 'owner@mail.com',
-  },
-  {
-    id: 'acd85619-3138-46b3-abef-0d20b6a3cd5e',
-    fullName: 'Mei mei',
-    isActive: true,
-    phone: '+6281234567890',
-    Role: { name: 'Super Admin' },
-    email: 'super.admin@mail.com',
-  },
-  {
-    id: '759cdf37-8ce4-45a8-8fc6-5d5c7c11d0cb',
-    fullName: 'Susanti',
-    isActive: true,
-    phone: '+6281234567890',
-    Role: { name: 'Admin' },
-    email: 'admin@mail.com',
-  },
-  {
-    id: 'a5bb8503-44d6-4219-be6e-3f295dd193fd',
-    fullName: 'Ijat',
-    isActive: false,
-    phone: null,
-    Role: { name: 'User' },
-    email: 'user@mail.com',
-  },
-  {
-    id: 'de8de118-82aa-4684-aea7-2a0da99e8508',
-    fullName: 'Tok Abah',
-    isActive: true,
-    phone: null,
-    Role: { name: 'Guest' },
-    email: 'guest@mail.com',
-  },
-  {
-    id: '0f802cd0-b669-4b12-a787-86e805e9d982',
-    fullName: 'Kak Ros',
-    isActive: true,
-    phone: null,
-    Role: { name: 'Owner' },
-    email: 'owner@mail.com',
-  },
-]
+  modalLabel: { width: 150, fontWeight: 600 },
+}))
 
 function UserPage() {
-  const columnHelper = createColumnHelper<UserRelationEntity>()
+  const [page, setPage] = useState(1)
+  const [pageSize, setPageSize] = useState(10)
+  const [name, setName] = useState(undefined)
 
-  const columns = [
-    columnHelper.accessor('id', {
-      header: 'No',
-      size: 10,
-      cell: (info) => info.row.index + 1,
-    }),
-    columnHelper.accessor('fullName', {
-      header: 'Fullname',
-      cell: (info) => info.getValue(),
-    }),
-    columnHelper.accessor('email', {
-      header: 'Email',
-      cell: (info) => info.getValue(),
-    }),
-    columnHelper.accessor('phone', {
-      header: 'Phone',
-      cell: (info) => {
-        const value = info.getValue()
-        return formatPhone(value)
-      },
-    }),
-    columnHelper.accessor('isActive', {
-      header: 'Active',
-      size: 10,
-      cell: (info) => {
-        const value = info.getValue()
-        return <Checkbox checked={value} readOnly />
-      },
-    }),
-    columnHelper.accessor('Role', {
-      header: 'Role',
-      cell: (info) => {
-        const value = info.getValue()
-        return value?.name
-      },
-    }),
-    columnHelper.accessor('id', {
-      header: 'Detail',
-      size: 10,
-      cell: (info) => {
-        const value = info.getValue()
-        console.log(value)
+  const debouncedName = useDebounce(name, 500)
 
-        return (
-          <ThemeIcon variant="light" radius="md" size="md" color="teal">
-            <IconEyeCheck />
-          </ThemeIcon>
-        )
-      },
-    }),
-    columnHelper.accessor('id', {
-      header: 'Edit',
-      size: 10,
-      cell: (info) => {
-        const value = info.getValue()
-        console.log(value)
+  const [selectedRecords, setSelectedRecords] = useState<UserRelationEntity[]>(
+    [],
+  )
 
-        return (
-          <ThemeIcon variant="light" radius="md" size="md">
-            <IconEdit />
-          </ThemeIcon>
-        )
-      },
-    }),
+  const { classes } = useStyles()
+
+  const queryUser = useUser()
+  const { data, refetch, isFetching, total, helpers } = queryUser
+  const newData = data ?? []
+
+  useEffect(() => {
+    refetch()
+  }, [])
+
+  useEffect(() => {
+    helpers.setQuery((helper) => {
+      helper.query.set('page', undefined)
+      helper.filtered.set('fullName', debouncedName)
+    })
+  }, [debouncedName])
+
+  const {
+    breakpoints: { xs: xsBreakpoint },
+  } = useMantineTheme()
+  const aboveXsMediaQuery = `(min-width: ${xsBreakpoint}px)`
+
+  const columns: DataTableColumn<UserRelationEntity>[] = [
+    {
+      accessor: 'fullName',
+      title: 'Fullname',
+      width: 150,
+      ellipsis: true,
+    },
+    {
+      accessor: 'email',
+      visibleMediaQuery: aboveXsMediaQuery,
+    },
+    {
+      accessor: 'phone',
+      title: 'Phone',
+      width: 170,
+      visibleMediaQuery: aboveXsMediaQuery,
+      render: (info) => formatPhone(info.phone),
+    },
+    {
+      accessor: 'isActive',
+      title: 'Active',
+      textAlignment: 'center',
+      width: 180,
+      render: (info) => <Checkbox checked={info.isActive} />,
+    },
+    {
+      accessor: 'isBlocked',
+      title: 'Blocked',
+      textAlignment: 'center',
+      width: 180,
+      render: (info) => <Checkbox checked={info.isBlocked} />,
+    },
+    {
+      accessor: 'Role.name',
+      title: 'Role',
+      width: 150,
+      visibleMediaQuery: aboveXsMediaQuery,
+    },
+    {
+      accessor: 'createdAt',
+      title: 'Register At',
+      textAlignment: 'right',
+      render: (info) => dayjs(info?.createdAt).format('DD MMMM YYYY'),
+    },
+    {
+      accessor: 'actions',
+      title: <Text mr="xs">Actions</Text>,
+      textAlignment: 'center',
+      render: (info) => (
+        <Group spacing={4} position="center" noWrap>
+          <ActionIcon color="blue" onClick={() => console.log('Edit', info.id)}>
+            <IconEdit size={18} />
+          </ActionIcon>
+          <ActionIcon
+            color="red"
+            onClick={() => console.log('Delete', info.id)}
+          >
+            <IconTrash size={18} />
+          </ActionIcon>
+        </Group>
+      ),
+    },
   ]
+
+  function showModalContent(info: UserRelationEntity) {
+    return openModal({
+      title: `User Detail`,
+      classNames: { modal: classes.modal, title: classes.modalTitle },
+      children: (
+        <div>
+          <Divider variant="dashed" />
+
+          <Stack mt="md">
+            <Group>
+              <Text className={classes.modalLabel} size="sm">
+                Fullname
+              </Text>
+              <Text size="sm">{info.fullName}</Text>
+            </Group>
+
+            <Group>
+              <Text className={classes.modalLabel} size="sm">
+                email
+              </Text>
+              <Text size="sm">{info.email}</Text>
+            </Group>
+
+            <Group>
+              <Text className={classes.modalLabel} size="sm">
+                Role
+              </Text>
+              <Text size="sm">{info.Role?.name}</Text>
+            </Group>
+
+            <Group>
+              <Text className={classes.modalLabel} size="sm">
+                Register At
+              </Text>
+              <Text size="sm">
+                {dayjs(info.createdAt).format('DD MMMM YYYY')}
+              </Text>
+            </Group>
+          </Stack>
+        </div>
+      ),
+    })
+  }
 
   return (
     <div>
       <Grid my={12}>
         <Grid.Col span={3}>
-          <Input icon={<IconSearch size={16} />} placeholder="Search..." />
+          <Input
+            icon={<IconSearch size={16} />}
+            placeholder="Search..."
+            onChange={(e: any) => setName(e.target.value)}
+          />
         </Grid.Col>
         <Grid.Col span="auto" />
         <Grid.Col span={2} style={{ textAlign: 'right' }}>
@@ -176,9 +202,70 @@ function UserPage() {
         </Grid.Col>
       </Grid>
 
-      <MyTable data={data} columns={columns} />
+      <Divider variant="dotted" />
 
-      <Pagination mt="md" total={10} position="right" />
+      <DataTable
+        withBorder={false}
+        borderRadius="sm"
+        withColumnBorders
+        striped={false}
+        highlightOnHover
+        verticalAlignment="top"
+        verticalSpacing="md"
+        horizontalSpacing="md"
+        fetching={isFetching}
+        columns={columns}
+        records={newData}
+        page={page}
+        onPageChange={setPage}
+        totalRecords={total}
+        recordsPerPage={pageSize}
+        selectedRecords={selectedRecords}
+        onSelectedRecordsChange={setSelectedRecords}
+        onRowClick={(info) => showModalContent(info)}
+        rowContextMenu={{
+          borderRadius: 'md',
+          items: (info) => [
+            {
+              key: 'edit',
+              icon: <IconEdit size={14} />,
+              title: `Edit`,
+              onClick: () =>
+                showNotification({
+                  color: 'orange',
+                  message: `Should edit this record`,
+                }),
+            },
+            {
+              key: 'delete',
+              title: `Delete`,
+              icon: <IconTrashX size={14} />,
+              color: 'red',
+              hidden: selectedRecords.length !== 0 && selectedRecords.length > 1,
+              onClick: () =>
+                showNotification({
+                  color: 'red',
+                  message: `Should delete this record`,
+                }),
+            },
+            { key: 'divider-1', divider: true },
+            {
+              key: 'deleteMany',
+              hidden:
+                selectedRecords.length <= 1 ||
+                !selectedRecords.map((r) => r.id).includes(info.id),
+              title: `Delete ${selectedRecords.length} selected records`,
+              icon: <IconTrash size={14} />,
+              color: 'red',
+              onClick: () =>
+                showNotification({
+                  color: 'red',
+                  message: `Should delete ${selectedRecords.length} records`,
+                }),
+            },
+          ],
+        }}
+      />
     </div>
   )
 }
