f98403aaf0959e3cdd85fbcbd9f40ce8a550e758
N. Fajri <fajriajjha@gmail.com>
2022-10-12 18:07:23 +0700

    fix: config env

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,1 +1,4 @@
 export const BRAND = process.env.BRAND ?? 'Nextine Admin'
+export const AXIOS_TIMEOUT = process.env.AXIOS_TIMEOUT ?? '5m'
+
+export const LOCAL_STORAGE_SESSION = process.env.LOCAL_STORAGE_SESSION ?? 'token_expresso'
