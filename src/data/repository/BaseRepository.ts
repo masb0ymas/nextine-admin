cd9a0abd3515763ce4939605d4059dba64f5faa2
N. Fajri <fajriajjha@gmail.com>
2022-10-12 18:08:02 +0700

    feat: add base repository

---
+++ src/data/repository/BaseRepository.ts
@@ -0,0 +1,133 @@
+import { BASE_API_URL } from '@core/constants/ConstBaseURL'
+import { AxiosInstance, AxiosResponse } from 'axios'
+import { LOCAL_STORAGE_SESSION } from 'config/env'
+import FetchApi from 'config/Fetcher'
+
+const Fetcher = new FetchApi(BASE_API_URL, LOCAL_STORAGE_SESSION)
+
+interface BaseRepositoryEntity {
+  endpoint: string
+}
+
+export interface MultipleIdsEntity {
+  ids: string | string[]
+}
+
+class BaseRepository {
+  public api: AxiosInstance
+
+  private endpoint: string
+
+  constructor(value: BaseRepositoryEntity) {
+    this.api = Fetcher.default
+    this.endpoint = value.endpoint
+  }
+
+  /**
+   *
+   * @param queryParams
+   * @returns
+   */
+  public async findAll(queryParams: string): Promise<AxiosResponse<any, any>> {
+    return this.api.get(`${this.endpoint}?${queryParams}`)
+  }
+
+  /**
+   *
+   * @param id
+   * @returns
+   */
+  public async findById(id: string): Promise<AxiosResponse<any, any>> {
+    return this.api.get(`${this.endpoint}/${id}`)
+  }
+
+  /**
+   *
+   * @param formData
+   * @returns
+   */
+  public async create<TData>(
+    formData: TData,
+  ): Promise<AxiosResponse<any, any>> {
+    return this.api.post(`${this.endpoint}`, formData)
+  }
+
+  /**
+   *
+   * @param id
+   * @param formData
+   * @returns
+   */
+  public async update<TData>(
+    id: string,
+    formData: TData,
+  ): Promise<AxiosResponse<any, any>> {
+    return this.api.put(`${this.endpoint}/${id}`, formData)
+  }
+
+  /**
+   *
+   * @param id
+   * @returns
+   */
+  public async restore(id: string): Promise<AxiosResponse<any, any>> {
+    return this.api.put(`${this.endpoint}/restore/${id}`)
+  }
+
+  /**
+   *
+   * @param id
+   * @returns
+   */
+  public async softDelete(id: string): Promise<AxiosResponse<any, any>> {
+    return this.api.delete(`${this.endpoint}/soft-delete/${id}`)
+  }
+
+  /**
+   *
+   * @param id
+   * @returns
+   */
+  public async forceDelete(id: string): Promise<AxiosResponse<any, any>> {
+    return this.api.delete(`${this.endpoint}/force-delete/${id}`)
+  }
+
+  /**
+   *
+   * @param data
+   * @returns
+   */
+  public async multipleRestore(
+    data: MultipleIdsEntity,
+  ): Promise<AxiosResponse<any, any>> {
+    return this.api.post(`${this.endpoint}/multiple/restore`, data)
+  }
+
+  /**
+   *
+   * @param data
+   * @returns
+   */
+  public async multipleSoftDelete(
+    data: MultipleIdsEntity,
+  ): Promise<AxiosResponse<any, any>> {
+    return this.api.post(`${this.endpoint}/multiple/soft-delete`, data)
+  }
+
+  /**
+   *
+   * @param data
+   * @returns
+   */
+  public async multipleForceDelete(
+    data: MultipleIdsEntity,
+  ): Promise<AxiosResponse<any, any>> {
+    const response = await this.api.post(
+      `${this.endpoint}/multiple/force-delete`,
+      data,
+    )
+    return response
+  }
+}
+
+export default BaseRepository
---
+++ src/data/repository/UserRepository.ts
@@ -0,0 +1,91 @@
+import { UserAttributes } from 'data/entities/User'
+import BaseRepository, { MultipleIdsEntity } from './BaseRepository'
+
+const repo = new BaseRepository({ endpoint: '/user' })
+
+class UserRepository {
+  public static api = repo.api
+
+  /**
+   *
+   * @param formData
+   * @returns
+   */
+  public static async create(formData: UserAttributes) {
+    const response = await repo.create(formData)
+    return response
+  }
+
+  /**
+   *
+   * @param id
+   * @param formData
+   * @returns
+   */
+  public static async update(id: string, formData: UserAttributes) {
+    const response = await repo.update(id, formData)
+    return response
+  }
+
+  /**
+   *
+   * @param id
+   * @returns
+   */
+  public static async restore(id: string) {
+    const response = await repo.restore(id)
+    return response
+  }
+
+  /**
+   *
+   * @param id
+   * @returns
+   */
+  public static async softDelete(id: string) {
+    const response = await repo.softDelete(id)
+    return response
+  }
+
+  /**
+   *
+   * @param id
+   * @returns
+   */
+  public static async forceDelete(id: string) {
+    const response = await repo.forceDelete(id)
+    return response
+  }
+
+  /**
+   *
+   * @param data
+   * @returns
+   */
+  public static async multipleRestore(data: MultipleIdsEntity) {
+    const response = await repo.multipleRestore(data)
+    return response
+  }
+
+  /**
+   *
+   * @param data
+   * @returns
+   */
+  public static async multipleSoftDelete(data: MultipleIdsEntity) {
+    const response = await repo.multipleSoftDelete(data)
+    return response
+  }
+
+  /**
+   *
+   * @param data
+   * @returns
+   */
+  public static async multipleForceDelete(data: MultipleIdsEntity) {
+    const response = await repo.multipleForceDelete(data)
+    return response
+  }
+}
+
+export default UserRepository
