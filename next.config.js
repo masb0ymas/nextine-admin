8300560d06880198058a5ceff2758140842125de
mamenesia <dedy.prasetyo.h@gmail.com>
2022-10-13 20:07:45 +0700

    fix: add url env in config

--- next.config.js
+++ next.config.js
@@ -9,6 +9,7 @@ module.exports = withBundleAnalyzer({
   },
   env: {
     BRAND: process.env.BRAND,
+    URL_ENV: process.env.URL_ENV,
     AXIOS_TIMEOUT: process.env.AXIOS_TIMEOUT,
     LOCAL_STORAGE_SESSION: process.env.LOCAL_STORAGE_SESSION,
   },
