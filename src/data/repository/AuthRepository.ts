80616d79f16fccab43d3ecf1f4d3eb925afb51c8
N. Fajri <fajriajjha@gmail.com>
2022-10-23 16:37:12 +0700

    feat: add auth repository

---
+++ src/data/repository/AuthRepository.ts
@@ -0,0 +1,30 @@
+import { LoginAttributes } from 'data/entities/User'
+import BaseRepository from './BaseRepository'
+
+const repo = new BaseRepository({ endpoint: '/auth' })
+
+class AuthRepository {
+  public static api = repo.api
+
+  /**
+   *
+   * @param formData
+   * @returns
+   */
+  public static async login(formData: LoginAttributes) {
+    const response = await repo.api.post('/auth/sign-in', formData)
+    return response
+  }
+
+  /**
+   *
+   * @param formData
+   * @returns
+   */
+  public static async logout(formData: any) {
+    const response = await repo.api.post('/logout', formData)
+    return response
+  }
+}
+
+export default AuthRepository
