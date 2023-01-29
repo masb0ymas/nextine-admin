68e898a1cca7276eb75ac553d4136bdbb7391ddf
masb0ymas <n.fajri@outlook.com>
2023-01-29 09:08:26 +0700

    feat: add constant status

---
+++ src/@core/constants/ConstStatus.ts
@@ -0,0 +1,19 @@
+const PENDING = 'PENDING'
+const SUCCESS = 'SUCCESS'
+const CANCELLED = 'CANCELLED'
+const REQUEST = 'REQUEST'
+const REJECTED = 'REJECTED'
+const EXPIRED = 'EXPIRED'
+const FAILED = 'FAILED'
+
+const ConstStatus = {
+  PENDING,
+  SUCCESS,
+  CANCELLED,
+  REQUEST,
+  REJECTED,
+  EXPIRED,
+  FAILED,
+}
+
+export default ConstStatus
