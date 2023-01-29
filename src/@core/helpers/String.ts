06dc6800ca229aeab1fb094ca3e97e8b9d18ca6a
masb0ymas <n.fajri@outlook.com>
2023-01-29 09:08:42 +0700

    fix: improve helper string

--- src/@core/helpers/String.ts
+++ src/@core/helpers/String.ts
@@ -1,6 +1,16 @@
 /* eslint-disable no-unsafe-optional-chaining */
-function capitalizeFirstLetter(string: string) {
-  return string.charAt(0).toUpperCase() + string.slice(1)?.split('-')?.join(' ')
-}
+export function capitalizeFirstLetter(string: string) {
+  const new_word =
+    string.charAt(0).toUpperCase() + string.slice(1)?.split('-')?.join(' ')
 
-export { capitalizeFirstLetter }
+  const split_word = new_word.split(' ')
+
+  for (let i = 0; i < split_word.length; i += 1) {
+    split_word[i] =
+      split_word[i].charAt(0).toUpperCase() + split_word[i].slice(1)
+  }
+
+  const result = split_word.join(' ')
+
+  return result
+}
