307f85150ca0deb54e661566cac6d1c20b4ab1e2
masb0ymas <n.fajri@outlook.com>
2023-01-29 09:08:34 +0700

    fix: improve helper formatter

--- src/@core/helpers/Formatter.ts
+++ src/@core/helpers/Formatter.ts
@@ -1,3 +1,4 @@
+import ConstStatus from '@core/constants/ConstStatus'
 import _ from 'lodash'
 
 /**
@@ -34,4 +35,74 @@ export function formatPercent(value: string | number): string {
   const result = `${percent} %`
 
   return result
+}
+
+/**
+ *
+ * @param status
+ * @returns
+ */
+export function filterColorStatus(status: string): string {
+  let colorStatus: string = 'white'
+
+  if (status === ConstStatus.PENDING) {
+    colorStatus = 'blue'
+  } else if (status === ConstStatus.SUCCESS) {
+    colorStatus = 'teal'
+  } else if (status === ConstStatus.CANCELLED) {
+    colorStatus = 'red'
+  }
+
+  return colorStatus
+}
+
+/**
+ *
+ * @param value
+ * @returns
+ */
+export function hideAccountNumber(value: string): string {
+  const valueLength = value.length
+  const hideLength = valueLength - 6
+
+  const startValue = value.slice(0, hideLength) // '81234'
+  const endValue = value.slice(hideLength + 5) // '890'
+
+  const result = `${startValue}*****${endValue}` // '+6281234***890'
+
+  return result
+}
+
+/**
+ *
+ * @param value
+ * @returns
+ */
+export function imageSource(value?: string | null) {
+  let imgSrc = '/static/images/no_image.jpg'
+
+  if (value) {
+    imgSrc = value
+  }
+
+  return imgSrc
+}
+
+/**
+ *
+ * @param value
+ * @returns
+ */
+export function validateURL(value: string): boolean {
+  const pattern = new RegExp(
+    '^(https?:\\/\\/)?' + // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+      '(\\#[-a-z\\d_]*)?$',
+    'i'
+  ) // fragment locator
+
+  return !!pattern.test(value)
 }
